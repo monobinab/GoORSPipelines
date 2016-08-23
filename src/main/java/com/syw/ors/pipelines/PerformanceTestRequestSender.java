876a3f86d1e384c44e853d99980de9ee79b426f5
Li Chen <Li.Chen@searshc.com>
2016-08-23 17:34:16 -0500

    ORS-144:Initial checkin of performance test Dataflow pipeline

---
+++ src/main/java/com/syw/ors/pipelines/PerformanceTestRequestSender.java
@@ -0,0 +1,63 @@
+package com.syw.ors.pipelines;
+
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.cloud.dataflow.sdk.Pipeline;
+import com.google.cloud.dataflow.sdk.io.BigQueryIO;
+import com.google.cloud.dataflow.sdk.options.Default;
+import com.google.cloud.dataflow.sdk.options.Description;
+import com.google.cloud.dataflow.sdk.options.PipelineOptions;
+import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
+import com.google.cloud.dataflow.sdk.transforms.DoFn;
+import com.google.cloud.dataflow.sdk.transforms.ParDo;
+import com.syw.ors.utilities.NetClient;
+
+public class PerformanceTestRequestSender {
+	public static class SendRequests extends DoFn<TableRow, String> {
+		private static final long serialVersionUID = 1L;
+		String URL;
+
+		@Override
+		public void processElement(ProcessContext c) {
+			String json = (String) c.element().get("data_json");
+			// "https://syw-ors-qa.appspot.com"
+			NetClient.PutHTTPS(URL, json);
+			c.output(json);
+		}
+
+		public SendRequests(String URL) {
+			this.URL = URL;
+		}
+	}
+
+	public static interface PerformanceTestRequestSenderOptions extends
+			PipelineOptions {
+		@Description("URL")
+		@Default.String("https://syw-ors-qa.appspot.com")
+		String getURL();
+		void setURL(String URL);
+
+		@Description("requestDate")
+		@Default.String("20160816")
+		String getRequestDate();
+		void setRequestDate(String requestDate);
+	}
+
+	public static void main(String[] args) {
+		// args[0] URL
+		// <URL>
+		PipelineOptionsFactory
+				.register(PerformanceTestRequestSenderOptions.class);
+		PerformanceTestRequestSenderOptions options = PipelineOptionsFactory
+				.fromArgs(args).withValidation()
+				.as(PerformanceTestRequestSenderOptions.class);
+		Pipeline p = Pipeline.create(options);
+
+		p.apply(BigQueryIO.Read.named(
+				"ClientRequestsPull" + options.getRequestDate()).from(
+				"syw-ors-1226:ors_logs.ors_client_requests_"
+						+ options.getRequestDate())).apply(
+				ParDo.of(new SendRequests(options.getURL())));
+		p.run();
+	}
+
+}
---
+++ src/main/java/com/syw/ors/utilities/NetClient.java
@@ -0,0 +1,133 @@
+package com.syw.ors.utilities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.io.*;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLPeerUnverifiedException;
+
+public class NetClient {
+
+	public static void Get(String url_str) {
+		try {
+			URL url = new URL(url_str);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				System.err.println("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public static void PutHTTPS(String url_str, String json) {
+		URL url;
+		try {
+
+			url = new URL(url_str);
+			HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+			con.setDoOutput(true);
+			con.setRequestMethod("PUT");
+			OutputStreamWriter out = new OutputStreamWriter(
+					con.getOutputStream());
+			out.write(json);
+			out.close();
+
+			// dump all cert info
+			print_https_cert(con);
+
+			// dump all the content
+			print_content(con);
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void print_https_cert(HttpsURLConnection con) {
+
+		if (con != null) {
+
+			try {
+
+				System.out.println("Response Code : " + con.getResponseCode());
+				System.out.println("Cipher Suite : " + con.getCipherSuite());
+				System.out.println("\n");
+
+				Certificate[] certs = con.getServerCertificates();
+				for (Certificate cert : certs) {
+					System.out.println("Cert Type : " + cert.getType());
+					System.out.println("Cert Hash Code : " + cert.hashCode());
+					System.out.println("Cert Public Key Algorithm : "
+							+ cert.getPublicKey().getAlgorithm());
+					System.out.println("Cert Public Key Format : "
+							+ cert.getPublicKey().getFormat());
+					System.out.println("\n");
+				}
+
+			} catch (SSLPeerUnverifiedException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	private static void print_content(HttpsURLConnection con) {
+		if (con != null) {
+
+			try {
+
+				System.out.println("****** Content of the URL ********");
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+
+				String input;
+
+				while ((input = br.readLine()) != null) {
+					System.out.println(input);
+				}
+				br.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+}
